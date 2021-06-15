17b5a79efd267b43b193509b9c3d6c345246b9ef
susil-123 <85786924+susil-123@users.noreply.github.com>
2021-06-15 09:56:55 +0530

    Create Employee.java
    program to print details of three employees

---
+++ Employee.java
@@ -0,0 +1,24 @@
+class Employee{
+public static void main(String[] args)
+{System.out.print("Name Year of joining Address " );
+E1();
+E2();
+E3();
+	
+}
+public static void E1()
+{ 
+String name="Robert ",add="64C- WallsStreet";
+int doj=1994;
+System.out.print(name+" "+doj+" "+add);}
+public static void E2()
+{ 
+String name2="Sam",add2="68D- WallsStreet";
+int doj2= 2000;
+System.out.print(name2+" "+doj2+" "+add2);}
+public static void E3()
+{ 
+String name3="John ",add3="26B- WallsStreet";
+int doj3=1999;
+System.out.print(name3+" "+doj3+" "+add3);}
+}
